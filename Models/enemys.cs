77a8d99e242b4e0db967cc94c1243fe0f815b199
Trenton <trentonhansen50@gmail.com>
2019-06-05 13:06:21 -0700

    more work to enemys

--- Models/enemys.cs
+++ Models/enemys.cs
@@ -1,20 +1,37 @@
 using System.Collections.Generic;
 namespace consolepirates.Models
 {
-    public class EnemySkip : Skipper
+    Random rand = new Random();
+    public class EnemySkipper : Skipper
     {
-        public EnemySkip();
+        public EnemySkipper()
+        {
+            cannons = rand.Next(1,3);
+            currentCargo = List<ILootable>(rand.Next(0,7)); //Is this correct? How would I do multiple items in cargo?
+        }
     }
     public class EnemyHauler : Hauler
     {
-        public EnemyHauler();
+        public EnemyHauler()
+        {
+            cannons = rand.Next(2,4);
+            currentCargo = List<ILootable>(rand.Next(0,7));
+        }
     }
     public class EnemyFighter : Fighter
     {
-        public EnemyFighter();
+        public EnemyFighter()
+        {
+            cannons = rand.Next(3,6);
+            currentCargo = List<ILootable>(rand.Next(0,7));
+        }
     }
     public class EnemyNautilous : Nautilous
     {
-        public EnemyNautilous();
+        public EnemyNautilous()
+        {
+            cannons = rand.Next(8,12);
+            currentCargo = List<ILootable>(rand.Next(0,7));
+        }
     }
 }
