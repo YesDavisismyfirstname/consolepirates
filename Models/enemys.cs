0cc221e0041bd9105c9edb2086a017e16600695b
Trenton <trentonhansen50@gmail.com>
2019-06-05 12:48:51 -0700

    added enemys

---
+++ Models/enemys.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+namespace consolepirates.Models
+{
+    public class EnemySkip : Skipper
+    {
+        public EnemySkip();
+    }
+    public class EnemyHauler : Hauler
+    {
+        public EnemyHauler();
+    }
+    public class EnemyFighter : Fighter
+    {
+        public EnemyFighter();
+    }
+    public class EnemyNautilous : Nautilous
+    {
+        public EnemyNautilous();
+    }
+}
