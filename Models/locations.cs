c48d7a7c78f0b91a95b223bf62287fbe390158f8
Davis L <davis.lantz@gmail.com>
2019-06-05 13:08:34 -0700

    erge prep get enemies

--- Models/locations.cs
+++ Models/locations.cs
@@ -4,24 +4,55 @@ namespace consolepirates.Models
     {
         abstract public class Location
         {
+            public string name;
+            public string locationArt;
             public string description;
             public List<Location> availableLocations;
-            public void displayLocation(string location){
-                System.Console.WriteLine(locationArt);
+            public void displayInfo(){
+                System.Console.Write(locationArt);
             }
         }
         public class WorldMap : Location, IEventable
         {
+            public List<Action> actions{get;set;}
             public List<City> cityList;
             public WorldMap(){
+                name = "World Map";
+
+                locationArt = @"
+###############################################
+                                 _,__        .:
+         Darwin <*  /        | \
+            .-./     |.     :  :,
+           /           '-._/     \_
+          /                '       \
+        .'                         *: Brisbane
+     .-'                             ;
+     |                               |
+     \                              /
+      |                            /
+Perth  \*        __.--._          /
+        \     _.'       \:.       |
+        >__,-'             \_/*_.-'
+                              Melbourne
+                             :--,
+                              '/
+###############################################
+                ");
+
                 description = "You can plan your next destination here.";
                 actions = new List<Action>();
+                foreach(Location loc in availableLocations){
+                    actions.Add(new Travel(loc));
+                }
+                actions.Add(new Travel(availableLocations))
                 cityList = new List<City>();
             }
         }
         public class City : Location, IEventable
         {
-            public City(){
+            public List<Action> actions{get;set;}
+            public City(string name, string description, string art){
                 description = "You can plunder here.";
                 actions = new List<Action>();
             }
